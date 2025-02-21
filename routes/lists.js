0d7b28e0412a52f1ebbd32603e257dd4cd04de34
rachel <rachelricedev@gmail.com>
2025-02-20 21:13:26 -0800

    added list route

---
+++ routes/lists.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const listController = require("../controllers/list");
+
+// Define routes and use the controller functions
+router.get('/', listController.getLists);
+router.post('/add', listController.addList);
+router.post('/update/:id', listController.updateList);
+router.delete('/delete/:id', listController.deleteList);
+
+module.exports = router;
