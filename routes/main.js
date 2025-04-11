f238a2eaf6be3a9b556085db026d7332fd083b4b
rachel <rachelricedev@gmail.com>
2025-04-10 21:13:56 -0700

    linked about page, fixed styling on homepage

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ const homeController = require("../controllers/home");
 
 //Main Routes 
 router.get("/", homeController.getIndex);
+router.get("/about", homeController.getAbout);
 
 
 module.exports = router;
