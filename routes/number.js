2a7b1efbcfe0c4bccca2f325a6ef6ec5c13eecaa
rachel <rachelricedev@gmail.com>
2025-05-31 16:08:47 -0700

    formatting number ejs

--- routes/number.js
+++ routes/number.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const numberController = require("../controllers/numberController");
 
 // Number Routes - NEED TO BE IMPLEMENTED
-router.get("/", rockController.getIndex);
-router.post("/", rockController.playGame);
+// router.get("/", rockController.getIndex);
+// router.post("/", rockController.playGame);
 
 module.exports = router;
