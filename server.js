876dede95d94b2c0e9862d2122ad50eea6a7377d
rachel <rachelricedev@gmail.com>
2025-06-07 09:16:47 -0700

    revised comments in server file for clarity

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// Load environment variables from config/.env file
 require("dotenv").config({ path: "./config/.env" });
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const connectDB = require("./config/database");
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const cors = require('cors');
-const logger = require("morgan");
+
+// Import core modules and middleware
+const express = require('express'); // Main Express framework
+const expressLayouts = require('express-ejs-layouts'); //Milldeware to support layout files with EJS
+const connectDB = require("./config/database"); // Database connection function
+const mongoose = require('mongoose'); // ODM to interact with MongoDB
+const methodOverride = require('method-override'); // Middleware to support PUT and DELETE methods in forms
+const cors = require('cors'); // Middleware to enable CORS - Cross-Origin Resource Sharing
+const logger = require("morgan"); // Logs HTTP requests to the console
+
+// Import route files
 const mainRoutes = require("./routes/main");
 const flipRoutes = require("./routes/flip");
 const rockRoutes = require("./routes/rock");
@@ -23,22 +28,21 @@ app.set('view engine', 'ejs')
 // Set EJS layout middleware
 app.use(expressLayouts);
 
-//Enable CORS
+//Enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
-//Use Morgan for logging
+//Use Morgan for logging incoming requests to the console
 app.use(logger("dev"));
 
-//Serve static folder
+//Serve static folder from public directory
 app.use(express.static('public'));
 
-// Method Override for PUT and DELETE requests
+// Method Override for PUT and DELETE requests in forms
 app.use(methodOverride('_method'));
 
-//Body parsing - parse results
+//Body parsing - parse results from form data
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
 
 //Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
