95fd45c180a0b92c5b66e1febb95037b3fb81b83
rachel <rachelricedev@gmail.com>
2025-06-12 19:39:36 -0700

    added comments to Item schema, removed redundant itemModel

--- models/Item.js
+++ models/Item.js
@@ -1,13 +1,18 @@
+// Import Mongoose library to define and work with MongoDB schemas
 const mongoose = require('mongoose')
 
+// Define the schema for an Item document in MongoDB
 const ItemSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
+    // Optional item description: string
     description: {
         type: String
     }
 })
 
+// Export the model 
+// The model is named 'Item' and is based on the ItemSchema
 module.exports = mongoose.model('Item', ItemSchema)
