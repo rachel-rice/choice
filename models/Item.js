018e215aa5d4846114ab30fc23a67b637d3b5cf6
rachel <rachelricedev@gmail.com>
2025-07-25 07:53:58 -0700

    updated Item schema for linking items to lists

--- models/Item.js
+++ models/Item.js
@@ -1,16 +1,24 @@
 // Import Mongoose library to define and work with MongoDB schemas
 const mongoose = require('mongoose')
 
-// Define the schema for an Item document in MongoDB
+// Define the schema for an Item
 const ItemSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-    // Optional item description: string
-    description: {
-        type: String
-    }
+    description: String,
+    //Reference to the List the item belongs to
+    list: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'List',
+    required: true // optional, but recommended to enforce linkage
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 // Export the model 
