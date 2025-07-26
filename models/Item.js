8eb9e10e494b1dd15c15931409a4c6b3f47cf073
rachel <rachelricedev@gmail.com>
2025-07-26 07:42:21 -0700

    added timestamps to item schema

--- models/Item.js
+++ models/Item.js
@@ -3,23 +3,10 @@ const mongoose = require('mongoose')
 
 // Define the schema for an Item
 const ItemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: String,
-    //Reference to the List the item belongs to
-    list: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'List',
-    required: true // optional, but recommended to enforce linkage
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-})
+  name: { type: String, required: true },
+  description: String,
+  list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true }
+}, { timestamps: true });
 
 // Export the model 
 // The model is named 'Item' and is based on the ItemSchema
