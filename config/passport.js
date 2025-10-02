1d5fb3ae6706a008460db8bf0831fdc805b27e39
rachel-rice <105083228+rachel-rice@users.noreply.github.com>
2025-10-01 21:26:07 -0700

    commented out oath strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
 module.exports = function(passport) {
