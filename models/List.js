19f5b3ecf719d61eda5fd7e52a0d8f76e14c492f
rachel-rice <105083228+rachel-rice@users.noreply.github.com>
2025-10-19 19:36:41 -0700

    feat(list): add userId field to schema for user linked lists

--- models/List.js
+++ models/List.js
@@ -7,16 +7,15 @@ const ListSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }] // reference Item documents
+  items: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Item' }
+  ], 
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 }, { timestamps: true });
-
-
-// const ListSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true
-//   }
-// }, { timestamps: true }); 
 
 // Export the model 
 module.exports = mongoose.model('List', ListSchema);
