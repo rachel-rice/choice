7f7b8c205f99fad6b730da97e254faebad42c531
rachel-rice <105083228+rachel-rice@users.noreply.github.com>
2026-03-09 20:01:53 -0700

    add res.locals middleware

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //Routes
 app.use("/", mainRoutes);
 app.use("/items", itemsRoutes);
